700e58dc7e39d30e4f705a158575b501de93cf54
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-11-07 22:49:50 -0400

    added resturants section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Events from "./Events/events.jsx";
 import Footer from "./Footer/footer.jsx";
 import Home from "./Home/home.jsx";
 import Navbar from "./Navbar/navbar.jsx";
+import Resturants from "./Resturants/resturants.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           < Home />
           < Events />
           <Accomodation />
+          <Resturants/>
           <Footer />
     </>
   )
