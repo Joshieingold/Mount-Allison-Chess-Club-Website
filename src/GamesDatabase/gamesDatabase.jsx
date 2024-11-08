d20054fc4c3f874eae6c59bc76cdfbb42ce58125
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-11-08 00:23:53 -0400

    cleaned up some things

--- src/GamesDatabase/gamesDatabase.jsx
+++ src/GamesDatabase/gamesDatabase.jsx
@@ -5,17 +5,18 @@ const GamesDatabase = () => {
             <h1 className="title database">Database of Games</h1>
             <div className="cardSection">
                 <div className="tournamentCard">
+                    <h3 className="tournamentTitle">Mount Allison Open Chess Championship 2023</h3>
+                    <a href="https://lichess.org/study/UshxlBum/hGj8sKb1">
+                        <button className="gamesLink">View Games</button>
+                    </a>
+                </div>
+                <div className="tournamentCard">
                     <h3 className="tournamentTitle">MACC Internal Rapid Chess Tournament 2023</h3>
                     <a href="https://lichess.org/study/SBpNjvdh">
                         <button className="gamesLink">View Games</button>
                     </a>
                 </div>
-                <div className="tournamentCard">
-                    <h3 className="tournamentTitle">Mount Allison Open Chess Championship 2023</h3>
-                    <a href="lichess.org/study/UshxlBum/hGj8sKb1">
-                        <button className="gamesLink">View Games</button>
-                    </a>
-                </div>
+
             </div>
         </div>
     );
