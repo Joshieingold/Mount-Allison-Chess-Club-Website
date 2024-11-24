e7acf21d9b4ea1fa097d8dabe742535e90f07a12
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-11-24 19:13:47 -0400

    I gave up on accomodation for now

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,6 @@ function App() {
                         <HomeSlider />
                         <Events />
                         <GamesDatabase />
-                        <Accomodation/>
                         <Resturants />
                     </>
                 } />
